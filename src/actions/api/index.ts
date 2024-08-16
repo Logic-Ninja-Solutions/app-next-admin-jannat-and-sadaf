ab561bdd118c39c6b350d427fcc6c1580ada4f03
Habib Shahzad <habibshehzad55@gmail.com>
2024-08-16 16:22:22 +0500

    (chore) update API

--- src/actions/api/index.ts
+++ src/actions/api/index.ts
@@ -3,19 +3,20 @@
 import axios from 'axios';
 import { getAccessToken } from '../auth/server';
 
-const baseUrl = 'https://jannatandsadaf.com/api/';
+const baseUrl = 'https://api.jannatandsadaf.com/api/';
+// const baseUrl = 'http://localhost:3000/api/';
 
 const serverInstance = axios.create({
-    baseURL: baseUrl,
-    // withCredentials: true,
+  baseURL: baseUrl,
+  // withCredentials: true,
 });
 
 serverInstance.interceptors.request.use(async (config) => {
-    const token = await getAccessToken();
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
+  const token = await getAccessToken();
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default serverInstance;
